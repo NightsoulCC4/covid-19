037157aa76314722f96015b173deaa6af185779d
Nightsoul <StickyCoffee8360@gmail.com>
2020-04-26 20:35:19 +0700

    commit

---
+++ js/index.js
@@ -0,0 +1,36 @@
+$(function () {
+
+    var url = "https://pomber.github.io/covid19/timeseries.json";
+
+    $.getJSON(url, function (result) {
+
+        var no = 1;
+        for (var country in result) {
+            if(`${country}`=='Taiwan*'){
+                var row = `<tr>
+                        <th scope="row">${no}</th>
+                        <td>
+                        <a href="country.html?country=${country}">${country}</a>
+                        </td>
+                    </tr>`;
+            $("#data").append(row);
+            no++;
+            } else {
+            var row = `<tr>
+                        <th scope="row">${no}</th>
+                        <td>
+                        <a href="country.html?country=${country}">${country}</a>
+                        </td>
+                    </tr>`;
+            $("#data").append(row);
+            no++;
+            }
+        }
+
+    });
+
+
+
+
+
+})
